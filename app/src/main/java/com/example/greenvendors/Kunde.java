c48770c55da9ce5e69ba7561a2302dc03f52de5c
Willan <Kamauwanjehia@gmail.com>
2019-09-05 14:12:34 -0700

    Green Vendorss

--- app/src/main/java/com/example/greenvendors/Kunde.java
+++ app/src/main/java/com/example/greenvendors/Kunde.java
@@ -1,17 +1,67 @@
 package com.example.greenvendors;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 public class Kunde extends AppCompatActivity {
 
-    @Override
+   int price=0;
+   int amount;
+   Button button1;
+   Button button2;
+   TextView textView1;
+   TextView textView2;
+   Button button3;
+
+   @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kunde);
-        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+
+
+
+        button1=findViewById(R.id.plus);
+        button2=findViewById(R.id.minus);
+        textView1=findViewById(R.id.text);
+        textView2=findViewById(R.id.amount);
+        button3=findViewById(R.id.cart);
+
+        textView1.setText("0");
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                price = price + 1;
+                amount=price*30;
+                textView1.setText(String.valueOf(price));
+                textView2.setText(String.valueOf(amount));
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (price >= 1) { price --; }{
+                    amount=price*30;
+                    textView1.setText(String.valueOf(price));
+                textView2.setText(String.valueOf(amount));}
+            }
+
+        });
+
+
+
+
+
+
     }
+
+
+
+
 }
--- app/src/main/java/com/example/greenvendors/MainActivity.java
+++ app/src/main/java/com/example/greenvendors/MainActivity.java
@@ -71,6 +71,7 @@ public class MainActivity extends AppCompatActivity {
             try {
                 // Google Sign In was successful, authenticate with Firebase
                 GoogleSignInAccount account = task.getResult(ApiException.class);
+                assert account != null;
                 firebaseAuthWithGoogle(account);
             } catch (ApiException e) {
                 // Google Sign In failed, update UI appropriately
